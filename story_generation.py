5b8c5986438373bae9371878ad13555ef3c4f816
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-23 23:45:22 -0400

    Added method to save and load train/test pairs via text files

--- story_generation.py
+++ story_generation.py
@@ -1,54 +1,89 @@
-# -*- coding: utf-8 -*-
+# story_generation.py
 
 from __future__ import unicode_literals, print_function, division
 from io import open
-import unicodedata
-import string
-import re
-import random
+from pathlib import Path
+
 import collections
+import random
+import re
+import string
+import unicodedata
 
-import torch
 import nltk
+import pymeteor.pymeteor as pymeteor
+import torch
 
-import storygen.seq2seq as seq2seq
-import storygen.book as book
-import pymeteor.pymeteor.pymeteor as pymeteor
-
-x = pymeteor.meteor('test', 'test2')
+from storygen import *
 
 #Consts
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 MAX_LENGTH = 1024
 HIDDEN_SIZE = 256
-ENCODER_FILE_FORMAT = "obj/encoder_%d.torch"
-DECODER_FILE_FORMAT = "obj/decoder_%d.torch"
+ENCODER_FILE_FORMAT = 'obj/encoder_%d.torch'
+DECODER_FILE_FORMAT = 'obj/decoder_%d.torch'
+TRAIN_TEST_DATA_FILE_FORMAT = 'data/%s_%d_%s.txt'
 
 # Read all the lines from a book and convert them to an array of sentences
-def getSentencesFromBook(book_title):
+def get_pairs(book_title, percentage):
 	print('Reading book...')
-	
-	lines = open('data/%s.txt' % book_title, encoding='utf-8').read().strip().split('\n')
-	lines = [book.normalizeString(line) for line in lines if len(line) > 0]
-	with open('data/contractions_dictionary.txt', 'r') as f:
-		s = f.read()
-		contractions = eval(s)
-	contraction_dict = book.ContractionDict(contractions)
-	
-	return book.convertLinesToSentences(lines, contraction_dict)
-
-# Convert all sentences into pairs and split into training and testing data
-def getPairs(sentences):
-	pairs = [[sentences[i], sentences[i+1]] for i,_ in enumerate(sentences[:len(sentences)-1])]
-	
-	random.shuffle(pairs)
-	train_size = int(len(pairs)*0.8)
-	train_pairs = pairs[:train_size]
-	test_pairs = pairs[train_size:]
-	
+	train_pairs = None
+	test_pairs = None
+	# Check if the train/test data exists for the given book with the given percentage
+	train_file = Path(TRAIN_TEST_DATA_FILE_FORMAT % (book_title, int(percentage*100), 'train'))
+	test_file = Path(TRAIN_TEST_DATA_FILE_FORMAT % (book_title, int(percentage*100), 'test'))
+	if train_file.is_file() and test_file.is_file():
+		print('Reading from file')
+		# Read train/test data from the files
+		train_data = train_file.open(mode='r').read().split('\n')
+		test_data = test_file.open(mode='r').read().split('\n')
+		print('len(train/test_data)=%d, %d'%(len(train_data), len(test_data)))
+		train_iter = iter(train_data)
+		train_pairs = [item for item in zip(train_iter, train_iter)]
+		test_iter = iter(test_data)
+		test_pairs = [item for item in zip(test_iter, test_iter)]
+	else:
+		print('Writing to file')
+		lines = open('data/%s.txt' % book_title, encoding='utf-8').read().strip().split('\n')
+		lines = [book.normalizeString(line) for line in lines if len(line) > 0]
+		with open('data/contractions_dictionary.txt', 'r') as f:
+			s = f.read()
+			contractions = eval(s)
+		contraction_dict = book.ContractionDict(contractions)
+		sentences = book.convertLinesToSentences(lines, contraction_dict)
+		# Convert all sentences into pairs and split into training and testing data 
+		sentences_iter = iter(sentences)
+		next(sentences_iter)
+		pairs = [(prev, curr) for prev, curr in zip(sentences, sentences_iter)]
+		random.shuffle(pairs)
+		train_size = int(len(pairs)*percentage)
+		train_pairs = pairs[:train_size]
+		test_pairs = pairs[train_size:]
+		# write the train/test pairs into files
+		# Condense the train/test pairs down into a singular list to write
+		train_data = [item for pair in train_pairs for item in pair]
+		test_data = [item for pair in test_pairs for item in pair]
+		# Write train data
+		f = train_file.open(mode='w')
+		for i, item in enumerate(train_data):
+			if i+1 < len(train_data):
+				f.write('%s\n'%item)
+			else:
+				f.write('%s'%item)
+		f.close()
+		# Write test data
+		f = test_file.open('w')
+		for i, item in enumerate(test_data):
+			if i+1 < len(test_data):
+				f.write('%s\n'%item)
+			else:
+				f.write('%s'%item)
+		f.close()
+	print('len(train/test)=%d, %d'%(len(train_pairs), len(test_pairs)))
+	print('train[0]=%s\ntest[0]=%s'%(str(train_pairs[0]), str(test_pairs[0])))
 	return train_pairs, test_pairs
 
-def getBooks(book_title, train_pairs, test_pairs):
+def get_books(book_title, train_pairs, test_pairs):
         input_book = book.Book("input_%s" % book_title)
         output_book = book.Book("output_%s" % book_title)
 
@@ -83,12 +118,10 @@ def createStory(input_sentence, input_book, output_book, encoder, decoder):
 def main():
 
 	book_title = '1_sorcerers_stone'
-
-	sentences = getSentencesFromBook(book_title)
 	
-	train_pairs, test_pairs = getPairs(sentences)
+	train_pairs, test_pairs = get_pairs(book_title, 0.8)
         
-	input_book, output_book = getBooks(book_title, train_pairs, test_pairs)
+	input_book, output_book = get_books(book_title, train_pairs, test_pairs)
 	
 	epoch_sizes = [25]#[100, 200, 400, 600]
 	for epoch_size in epoch_sizes:
