d226c45e6783cf1780d01a98c10d6f50f35b4d34
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-23 22:05:04 -0400

    Initial commit

---
+++ storygen/decoder.py
@@ -0,0 +1,60 @@
+# decoder.py
+
+######################################################################
+# Attention Decoder
+# -----------------
+# The decoder is another RNN that takes the encoder output vector(s) and
+# outputs a sequence of words to create the translation.
+#
+# Attention allows the decoder network to "focus" on a different part of
+# the encoder's outputs for every step of the decoder's own outputs. First
+# we calculate a set of *attention weights*. These will be multiplied by
+# the encoder output vectors to create a weighted combination. The result
+# (called ``attn_applied`` in the code) should contain information about
+# that specific part of the input sequence, and thus help the decoder
+# choose the right output words.
+#
+# Calculating the attention weights is done with another feed-forward
+# layer ``attn``, using the decoder's input and hidden state as inputs.
+# Because there are sentences of all sizes in the training data, to
+# actually create and train this layer we have to choose a maximum
+# sentence length (input length, for encoder outputs) that it can apply
+# to. Sentences of the maximum length will use all the attention weights,
+# while shorter sentences will only use the first few.
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, max_length, dropout_p=0.1):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        # log_softmax uses log base e
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self, device):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
---
+++ storygen/encoder.py
@@ -0,0 +1,27 @@
+# encoder.py
+
+######################################################################
+# The Encoder
+# -----------
+#
+# The encoder of a seq2seq network is a RNN that outputs some value for
+# every word from the input sentence. For every input word the encoder
+# outputs a vector and a hidden state, and uses the hidden state for the
+# next input word.
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self, device):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
