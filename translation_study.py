a8a0e2f2ec558f83ed457b338a47b07c172f7863
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-11-07 15:27:13 -0500

    Updated method parameters

--- translation_study.py
+++ translation_study.py
@@ -16,16 +16,16 @@ def main():
     train_pairs, test_pairs = storygen.getPairs(sentences)
     input_book, output_book = storygen.getBooks(book_title, train_pairs, test_pairs)
     
-    epoch_size = 0
+    epoch_size = 1
     
     encoder_filename = storygen.ENCODER_FILE_FORMAT % epoch_size
     decoder_filename = storygen.DECODER_FILE_FORMAT % epoch_size
 
-    ITER_AMOUNT = 10000
+    EPOCHS = 100
     
     network = seq2seq.Seq2Seq(train_pairs, test_pairs, storygen.MAX_LENGTH, storygen.HIDDEN_SIZE, DEVICE)
     if not network.loadFromFiles(encoder_filename, decoder_filename):
-        network.trainIters(ITER_AMOUNT, input_book, output_book)
+        network.train_model(train_pairs, EPOCHS)
         network.saveToFiles(encoder_filename, decoder_filename)
     # end story_generation.py code
 
@@ -43,17 +43,17 @@ def main():
     bleu_random_score = 0.0
     meteor_random_score = 0.0
     for pair in pairs:
-        output_words, attentions = network.evaluate(pair[0], input_book, output_book)
+        output_words, attentions = network.evaluate(pair[0])
         output_sentence = ' '.join(output_words)
         
         bleu_predicted_score += seq2seq.calculateBleu(output_sentence, pair[1])
         # getting errors if the two strings have 0 matching words...
-        #meteor_predicted_score += pymeteor.meteor(output_sentence, pair[1])
+        meteor_predicted_score += pymeteor.meteor(output_sentence, pair[1])
 
         random_sentence = ' '.join([random.sample(words, 1)[0] for i in range(len(output_words))])
 
         bleu_random_score += seq2seq.calculateBleu(random_sentence, pair[1])
-        #meteor_random_score += pymeteor.meteor(random_sentence, pair[1])
+        meteor_random_score += pymeteor.meteor(random_sentence, pair[1])
         
     bleu_predicted_score /= len(pairs)
     meteor_predicted_score /= len(pairs)
